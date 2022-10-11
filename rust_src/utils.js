074866142031ca7aeeb0f31979f5e5ac453ac0d9
Bjarne <bjarne@morningscore.io>
2022-10-11 14:53:49 +0200

    cleanup

--- rust_src/utils.js
+++
@@ -1,7 +0,0 @@
-import utils from '../crates/utils'
-
-export const isDate = d => d instanceof Date;
-export const isEmpty = o => utils.isEmpty(o);
-export const isObject = o => o != null && typeof o === 'object';
-export const hasOwnProperty = (o, ...args) => Object.prototype.hasOwnProperty.call(o, ...args)
-export const isEmptyObject = (o) => isObject(o) && isEmpty(o);
--- test/added.bench.js
+++ test/added.bench.js
@@ -1,5 +1,4 @@
 import addedDiff from '../src/added';
-import rustAddedDiff from '../rust_src/added';
 import { benchmarkSuite } from "jest-bench";
 
 benchmarkSuite('.addedDiff', {
@@ -36,22 +35,5 @@ benchmarkSuite('.addedDiff', {
         expect(addedDiff(type, value)).toEqual({});
       })
     },
-
-    ['rust_equal']: () => {
-
-      [
-        ['int', 1],
-        ['string', 'a'],
-        ['boolean', true],
-        ['null', null],
-        ['undefined', undefined],
-        ['object', { a: 1 }],
-        ['array', [1]],
-        ['function', () => ({})],
-        ['date', new Date()],
-      ].forEach(([type, value]) => {
-        expect(rustAddedDiff(type, value)).toEqual({});
-      })
-    }
 
 });
