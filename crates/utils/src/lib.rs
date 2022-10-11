5034211a7544da3a3fce824fa1b7e78abbfbf305
Bjarne <bjarne@morningscore.io>
2022-10-11 13:20:52 +0200

    Added deleted_diff

--- crates/utils/src/lib.rs
+++ crates/utils/src/lib.rs
@@ -91,11 +91,80 @@ fn added_diff<'a>(
     Ok(acc)
 }
 
+fn deleted_diff<'a>(
+    cx: &mut impl Context<'a>,
+    lhs: Handle<'a, JsValue>,
+    rhs: Handle<'a, JsValue>,
+) -> JsResult<'a, JsObject> {
+    if rhs.strict_equals(cx, lhs) {
+        return Ok(cx.empty_object());
+    }
+
+    if rhs.is_a::<JsObject, _>(cx) == false {
+        return Ok(cx.empty_object());
+    }
+
+    if lhs.is_a::<JsObject, _>(cx) == false {
+        return Ok(cx.empty_object());
+    }
+
+    let r: Handle<JsObject> = js_value_to_object(cx, rhs)?;
+    let l: Handle<JsObject> = js_value_to_object(cx, lhs)?;
+
+    let r_keys: Handle<JsArray> = get_object_keys(cx, r)?;
+    let l_keys: Handle<JsArray> = get_object_keys(cx, l)?;
+
+    let r_keys_vec: Vec<Handle<JsValue>> = r_keys.to_vec(cx)?;
+    let l_keys_vec: Vec<Handle<JsValue>> = l_keys.to_vec(cx)?;
+
+    let acc = cx.empty_object();
+
+    for (_i, l_key) in l_keys_vec.iter().enumerate() {
+        let mut r_has_l_key: bool = false;
+        let key_as_string = js_value_to_string(cx, *l_key)?;
+        let l_val: Handle<JsValue> = l.get(cx, key_as_string)?;
+
+        for (_i, r_key) in r_keys_vec.iter().enumerate() {
+            let r_key_as_string = js_value_to_string(cx, *r_key)?;
+            if r_key_as_string.strict_equals(cx, key_as_string) {
+                r_has_l_key = true;
+            }
+        }
+
+        if r_has_l_key {
+            let r_val: Handle<JsValue> = r.get(cx, key_as_string)?;
+            let diff: Handle<JsObject> = deleted_diff(cx, l_val, r_val)?;
+
+            if diff.is_a::<JsObject, _>(cx) == true {
+                let is_diff_empty: Handle<JsBoolean> = is_object_empty(cx, diff)?;
+                let false_check = cx.boolean(false);
+                if is_diff_empty.strict_equals(cx, false_check) {
+                    acc.set(cx, key_as_string, diff)?;
+                }
+            } else {
+                acc.set(cx, key_as_string, diff)?;
+            }
+        } else {
+            let undefined_value = cx.undefined();
+            acc.set(cx, key_as_string, undefined_value)?;
+        }
+    }
+
+    Ok(acc)
+}
+
 fn bind_added_diff(mut cx: FunctionContext) -> JsResult<JsObject> {
     let lhs: Handle<JsValue> = cx.argument::<JsValue>(0)?;
     let rhs: Handle<JsValue> = cx.argument::<JsValue>(1)?;
 
     added_diff(&mut cx, lhs, rhs)
+}
+
+fn bind_deleted_diff(mut cx: FunctionContext) -> JsResult<JsObject> {
+    let lhs: Handle<JsValue> = cx.argument::<JsValue>(0)?;
+    let rhs: Handle<JsValue> = cx.argument::<JsValue>(1)?;
+
+    deleted_diff(&mut cx, lhs, rhs)
 }
 
 fn bind_is_empty(
@@ -111,5 +180,6 @@ fn bind_is_empty(
 fn main(mut cx: ModuleContext) -> NeonResult<()> {
     cx.export_function("isEmpty", bind_is_empty)?;
     cx.export_function("addedDiff", bind_added_diff)?;
+    cx.export_function("deleted", bind_deleted_diff)?;
     Ok(())
 }
